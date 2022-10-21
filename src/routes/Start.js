16d69f94336dce11fe6d82fee7aec5090238a08d
FrankAsplund <frank_asplund@hotmail.com>
2022-10-21 19:23:19 +0200

    Unfinished Start page + small css fixes

--- src/routes/Start.js
+++ src/routes/Start.js
@@ -1,17 +1,34 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import logga3 from "../components/assets/logga3.png";
+import earth from "../components/assets/earth.png";
+import recycle from "../components/assets/recycle.png";
+import circlecutlery from "../components/assets/circlecutlery.png";
 import "./Start.css";
 
 export const Start = () => {
   return (
     <div className="startBG">
+      <div className="container">
+        <img id="logga" src={logga3} />
+        <h1 id="logoName">Svinneriet</h1>
+      </div>
       <div className="border">
         <h1 id="vinn">Vinn med svinn</h1>
         <p id="text1">
           Rädda klimatet, spara pengar, och få en utsökt måltid på en och samma
           gång!
         </p>
+        <div className="symbolGrid">
+          <img id="earth" src={earth}></img>
+          <img id="recycle" src={recycle}></img>
+          <img id="circlecutlery" src={circlecutlery}></img>
+        </div>
+
+        <Link to="/">
+          <p className="subText2">Kom igång!</p>
+        </Link>
       </div>
     </div>
   );
