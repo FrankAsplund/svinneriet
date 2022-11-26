da8e753e6252cd8ce195e391a48a298980a5814c
FrankAsplund <frank_asplund@hotmail.com>
2022-11-26 19:36:38 +0100

    Error handling if store is in active subs, fixed

--- src/components/Modal/Dashboard.js
+++ src/components/Modal/Dashboard.js
@@ -23,14 +23,14 @@ class Dashboard extends Component {
     return (
       <main>
         <Modal show={this.state.show} handleClose={this.hideModal}>
-          <div className="modalClass">
+          {/* <div className="modalClass">
             <p className="modalText1">
               Är du säker på att du vill prenumerera på denna tjänst?
             </p>
             <p className="modalText2">
               99:- kommer dras på autogiro varje månad, är du säker?
             </p>
-          </div>
+          </div> */}
         </Modal>
         <button type="button" className="text2" onClick={this.showModal}>
           Prenumerera!
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -16,6 +16,7 @@ const Modal = ({ handleClose, show, children }) => {
   const [subs, setSubs] = useState({});
   var storeID = "";
   var match = false;
+  var subsArray = [];
 
   const navigate = useNavigate();
 
@@ -31,34 +32,22 @@ const Modal = ({ handleClose, show, children }) => {
       try {
         const { data } = await axios.get(`http://localhost:8000/stores/${id}`);
         setStore(data);
-        storeID = data.id;
-        console.log("STOREGET - storeID: ", storeID);
       } catch (err) {
         console.error(err);
       }
     };
-
     fetch();
   };
 
+  /* Fetching the active subcriptions */
   const SubsGet = () => {
     const fetch = async () => {
       try {
         const { data } = await axios.get("http://localhost:8000/activeSubs");
         setSubs(data);
         console.log("subs:", data);
-        console.log("SUBSGET - storeID", storeID);
-
-        for (var i = 0; i < data.length; i++) {
-          if (data[i].id == storeID) {
-            console.log("The id matches.");
-            match = true;
-            return;
-          } else {
-            console.log("The id didn't match.");
-            return;
-          }
-        }
+        subsArray = data;
+        console.log("subsArray", subsArray);
       } catch (err) {
         console.error(err);
       }
@@ -66,14 +55,51 @@ const Modal = ({ handleClose, show, children }) => {
     fetch();
   };
 
+  /* Checks if the the store already is in your subscriptions. If it is, it changes "match" to false */
+  function matchCheck() {
+    console.log("matchCheck");
+    console.log("subs: ", subs);
+
+    storeID = store.id;
+    console.log("StoreID: ", storeID);
+
+    if (subs.length !== 0) {
+      for (var i = 0; i < subs.length; i++) {
+        if (subs[i].id === storeID) {
+          console.log("The id matches. ", subs[i].id);
+          match = true;
+
+          console.log(match);
+        } else if (subs[i].id !== storeID || null || "" || []) {
+          console.log("The id didn't match. ", subs[i].id);
+          match = false;
+
+          console.log(match);
+        }
+      }
+    }
+  }
+
   /* Creates an entry into the "activesubs" JSON-database based on the current ID of the page 
   (which is the same id as the store)
   , and then takes you to the Done/${id}-page, which is also based on the current ID of the page. */
   const postData = (e) => {
-    if ((match = true)) {
+    matchCheck();
+    console.log("postData", match);
+    if (match === true) {
       console.log("Denna affären finns redan i dina prenumerationer.");
+
+      var elem1 = document.getElementById("jaButton");
+      document.getElementById("nejButton").innerHTML = "Stäng";
+      var elem3 = document.getElementById("modalText2ID");
+      elem1.remove();
+      elem3.remove();
+
+      document.getElementById("modalText2ID").innerHTML =
+        "Denna affären finns redan i dina prenumerationer.";
+
       return;
-    } else {
+    } else if (match === false) {
       e.preventDefault();
       axios
         .post("http://localhost:8000/activeSubs", {
@@ -98,11 +124,27 @@ const Modal = ({ handleClose, show, children }) => {
     <div className={showHideClassName}>
       <section className="modal-main">
         {children}
+        <div className="modalClass">
+          <p className="modalText1" id="modalText2ID">
+            Är du säker på att du vill prenumerera på denna tjänst?
+          </p>
+          <p className="modalText2" id="modalText2ID">
+            99:- kommer dras på autogiro varje månad, är du säker?
+          </p>
+        </div>
         <div className="buttonClass">
-          <button onClick={postData} className="ja">
+          {/* <button onClick={matchCheck} className="ja" id="jaButton">
+            Check
+          </button> */}
+          <button onClick={postData} className="ja" id="jaButton">
             Ja
           </button>
-          <button className="nej" type="button" onClick={handleClose}>
+          <button
+            className="nej"
+            type="button"
+            onClick={handleClose}
+            id="nejButton"
+          >
             Nej
           </button>
         </div>
